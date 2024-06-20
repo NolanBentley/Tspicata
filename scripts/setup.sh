39f8b09ce643e8fe5af7fe28f22348c75fcf5a21
NolanBentley <NolanBentley@gmail.com>
2024-06-20 11:12:00 -0500

    Finished organizing and modifyuing scripts paths to reflect new structure.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,9 @@
 cd ~/Experiments/Tspicata/
 git init
-
+echo '# Compiled source #
+###################
+*_ignored' > .gitignore
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:NolanBentley/Tspicata.git
+git push -u origin main
