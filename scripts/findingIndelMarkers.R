fe3ac0771f06129d67ee6f95f821c2fb7a07199c
NolanBentley <NolanBentley@gmail.com>
2024-06-24 14:21:29 -0500

    Prpogated new results after fixing bug in finding indels int he original vcf. Also, lessened filtering on indels.

--- scripts/findingIndelMarkers.R
+++ scripts/findingIndelMarkers.R
@@ -1,15 +1,14 @@
 #Setup analysis
 setwd("~/Experiments/Tspicata/")
-indelFile <- "./data/Tspicata_both.d8.merged_indelSubset.csv"
+indelFile <- "./data/data_ignored/Tspicata_both.d8.merged_indelSubset.csv"
 
 #Load data
-library(data.table)
 df1 <- read.csv(indelFile)
 colnames(df1)[colnames(df1)=="X.CHROM"]<-"CHROM"
-dfOrig <- df1
+dfOrig<-df1
 
 #Recalculate alt allele length
-maxAlleleLen <- function(x){
+maxAlleleDiff <- function(x){
   y <-nchar(unlist(strsplit(x,split = ",")))
   maxInd <- which.max(y)
   zMax <- y[maxInd]
@@ -21,7 +20,7 @@ maxAlleleLen <- function(x){
   }
 }
 alleleVec <- paste0(df1$REF,",",df1$ALT)
-df1$altLen <- sapply(alleleVec,maxAlleleLen)
+df1$alleleDiff <- sapply(alleleVec,maxAlleleDiff)
 
 #Set filters
 opt<-list()
@@ -29,21 +28,21 @@ opt$notSamples <- c('X.CHROM','CHROM','POS','ID','REF','ALT','QUAL','FILTER','IN
 opt$startCol <- 10
 opt$maxMiss <- 0.5
 opt$minHomo <- 0.8
-opt$minAlleleDiff <- 12
-opt$outName <- paste0(
-  gsub("\\....$","",indelFile),
+opt$minAlleleDiff <- 10
+opt$outDir  <- "./data/"
+opt$outName <- file.path(opt$outDir,paste0(
+  gsub("\\.csv$","",basename(indelFile)),
   "_lt",opt$maxMiss,"Miss",
   "_gt",opt$minAlleleDiff-1,"bpDiff",
-  "_gt",opt$minHomo,"Homo",
   ".csv"
-)
-
+))
 
 ##Filtering calculations
 dfCalls <- df1[,!colnames(dfOrig)%in%opt$notSamples]
-df1$missFreq  <- rowMeans(dfCalls=="./.")
+dfCalls[dfCalls=="./."]<-NA
+df1$missFreq  <- rowMeans(is.na(dfCalls))
 hetGenos     <- apply(combn(0:4,2),2,paste,collapse="/")
-df1$homoFreq <- rowMeans(!apply(dfCalls,2,`%in%`,hetGenos))
+df1$hetFreq <- 1-rowMeans(!apply(dfCalls,2,`%in%`,hetGenos))
 
 updateKept <- function(prevLogic,logicFun){
   afterLogic <- prevLogic & logicFun
@@ -59,13 +58,11 @@ df1$kept1 <- updateKept(df1$kept1,logicFun = df1$missLogic)
 ## Check min allele length
 df1$minLenLogic <- df1$altLen>=opt$minAlleleDiff
 df1$kept1 <- updateKept(df1$kept1,logicFun = df1$minLenLogic)
+
 ## Check for biallelic
 df1$biallelic <- !grepl(",",df1$ALT)
 df1$kept1 <- updateKept(df1$kept1,logicFun = df1$biallelic)
-## Check heterozygosity
-df1$homoFreqLogic <- df1$homoFreq>=opt$minHomo
-df1$kept2 <- updateKept(df1$kept1,logicFun = df1$homoFreq>=opt$minHomo)
 
 #Out name
-write.csv(x = df1[df1$kept2,],file = opt$outName)
+write.csv(x = df1[df1$kept1,],file = opt$outName)
 
