96da1775ca3d2517b81e4a64fa3b73ead7f508ef
NolanBentley <NolanBentley@gmail.com>
2024-06-24 12:58:29 -0500

    Made the indel searching no longer omit insertions in the reference

--- scripts/subsettingToIndels.R
+++ scripts/subsettingToIndels.R
@@ -1,6 +1,6 @@
 #Setup analysis
 setwd("~/Experiments/Tspicata/")
-vcfFile      <- "./data_ignored/Tspicata_both.d8.merged.vcf"
+vcfFile      <- "./data/data_ignored/Tspicata_both.d8.merged.vcf"
 indelFileOut <- paste0(gsub("\\.vcf","",vcfFile),"_indelSubset.csv")
 
 #Load data
@@ -9,8 +9,9 @@ headerRow <- grep("#CHROM",readLines(vcfFile,n = 1000))
 vcf <- fread(vcfFile,skip = headerRow-1)
 
 #Remove SNPs
+vcf$refLen <- nchar(vcf$REF)
 vcf$altLen <- nchar(vcf$ALT)
-vcf<-vcf[vcf$altLen>1,]
+vcf<-vcf[vcf$altLen!=vcf$refLen,]
 
 #Save file
 write.table(x = vcf,indelFileOut,row.names = F,sep = ",")
